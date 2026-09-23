cbfcb7805ab92b7a36f3bc61b8269ef3ede9a3db
a.dernovskiy <dernovski@icloud.com>
2026-09-23 18:44:24 +0500

    Add image endpoint

---
+++ src/main/java/ru/yandex/practicum/mymarket/controller/ImageController.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.mymarket.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.mymarket.service.ImageService;
+
+import java.time.Duration;
+
+/**
+ * Выдача изображений товаров.
+ */
+@RestController
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @GetMapping("/images/{fileName}")
+    public ResponseEntity<Resource> getImage(@PathVariable String fileName) {
+        return imageService.load(fileName)
+                .map(image -> ResponseEntity.ok()
+                        .contentType(MediaTypeFactory.getMediaType(fileName)
+                                .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                        .cacheControl(CacheControl.maxAge(Duration.ofHours(1)))
+                        .body(image))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
