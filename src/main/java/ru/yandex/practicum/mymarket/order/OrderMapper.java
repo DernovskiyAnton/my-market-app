f89292ebc76915bf4e3a59e983f5ed1d01c97483
a.dernovskiy <dernovski@icloud.com>
2026-09-25 10:14:29 +0500

    Rewrite order service with reactive loading of order items

--- src/main/java/ru/yandex/practicum/mymarket/order/OrderMapper.java
+++ src/main/java/ru/yandex/practicum/mymarket/order/OrderMapper.java
@@ -1,17 +1,18 @@
 package ru.yandex.practicum.mymarket.order;
 
+import java.util.List;
+
 public final class OrderMapper {
 
     private OrderMapper() {
     }
 
-    public static OrderDto toDto(Order order) {
-        return new OrderDto(order.getId(), order.getItems().stream().map(OrderMapper::toDto).toList(),
-                order.getTotalSum());
+    public static OrderDto toDto(Order order, List<OrderItem> items) {
+        return new OrderDto(order.getId(), items.stream().map(OrderMapper::toDto).toList(), order.getTotalSum());
     }
 
     public static OrderItemDto toDto(OrderItem orderItem) {
-        return new OrderItemDto(orderItem.getItem().getId(), orderItem.getTitle(), orderItem.getPrice(),
+        return new OrderItemDto(orderItem.getItemId(), orderItem.getTitle(), orderItem.getPrice(),
                 orderItem.getQuantity());
     }
 }
