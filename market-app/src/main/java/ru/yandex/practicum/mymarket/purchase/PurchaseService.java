64d86456657ca162e6c9eaec05944ff6947817e4
a.dernovskiy <dernovski@icloud.com>
2026-09-25 12:46:51 +0500

    Pay for order in the same transaction as order creation

--- market-app/src/main/java/ru/yandex/practicum/mymarket/purchase/PurchaseService.java
+++ market-app/src/main/java/ru/yandex/practicum/mymarket/purchase/PurchaseService.java
@@ -10,6 +10,7 @@ import ru.yandex.practicum.mymarket.common.EmptyCartException;
 import ru.yandex.practicum.mymarket.order.Order;
 import ru.yandex.practicum.mymarket.order.OrderItem;
 import ru.yandex.practicum.mymarket.order.OrderService;
+import ru.yandex.practicum.mymarket.payment.PaymentService;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
@@ -21,17 +22,18 @@ public class PurchaseService {
 
     private final CartService cartService;
     private final OrderService orderService;
+    private final PaymentService paymentService;
     private final Clock clock;
 
     @Transactional
     public Mono<Long> buy() {
         return cartService.getCartLines()
                 .collectList()
-                .flatMap(this::createOrder)
+                .flatMap(this::placeOrder)
                 .flatMap(orderId -> cartService.clear().thenReturn(orderId));
     }
 
-    private Mono<Long> createOrder(List<CartLine> lines) {
+    private Mono<Long> placeOrder(List<CartLine> lines) {
         if (lines.isEmpty()) {
             return Mono.error(new EmptyCartException());
         }
@@ -39,6 +41,7 @@ public class PurchaseService {
                 .map(line -> new OrderItem(line.item(), line.quantity()))
                 .toList();
         long totalSum = items.stream().mapToLong(OrderItem::getSum).sum();
-        return orderService.create(new Order(LocalDateTime.now(clock), totalSum), items);
+        return orderService.create(new Order(LocalDateTime.now(clock), totalSum), items)
+                .flatMap(orderId -> paymentService.pay(totalSum).thenReturn(orderId));
     }
 }
