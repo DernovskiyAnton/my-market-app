d9cf55b4611fcf2138ac4e4ea5a18123d0f5b0ef
a.dernovskiy <dernovski@icloud.com>
2026-09-25 10:13:28 +0500

    Map entities with Spring Data Relational instead of JPA

--- src/main/java/ru/yandex/practicum/mymarket/order/Order.java
+++ src/main/java/ru/yandex/practicum/mymarket/order/Order.java
@@ -1,50 +1,28 @@
 package ru.yandex.practicum.mymarket.order;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-@Entity
-@Table(name = "orders")
+@Table("orders")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "total_sum", nullable = false)
     private long totalSum;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    @OrderBy("id")
-    private List<OrderItem> items = new ArrayList<>();
-
-    public Order(LocalDateTime createdAt) {
+    public Order(LocalDateTime createdAt, long totalSum) {
         this.createdAt = createdAt;
-    }
-
-    public void addItem(OrderItem orderItem) {
-        orderItem.setOrder(this);
-        items.add(orderItem);
-        totalSum += orderItem.getPrice() * orderItem.getQuantity();
+        this.totalSum = totalSum;
     }
 }
